3fa166aeea9951144004b04d11b965741dbed54a
cyee00 <cyee00@stuy.edu>
2018-10-23 09:51:01 -0400

    Class

--- SuperArray.java
+++ SuperArray.java
@@ -2,6 +2,15 @@ public class SuperArray {
   private String[] data;
   private int size;
   public SuperArray() {
-    String[] data = new String[];
+    String[] data = new String[10];
+  }
+  public void clear() {
+    size = 0;
+  }
+  public int size() {
+    return size;
+  }
+  public boolean isEmpty() {
+    return (size==0);
   }
 }
